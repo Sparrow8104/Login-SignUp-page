11b494ed35152f5a1005f0d1939045dba20c6add
Sparrow8104 <anchalsingh8102004@gmail.com>
2024-12-10 15:18:28 +0530

    changed statusCode

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use('/user',userRoutes)
 app.use((error,req,res,next) =>{
     
      const message=error.message || 'server error'
-     const statusCode=error.statusCode|| '500'
+     const statusCode=error.statusCode|| 500
 
      res.status(statusCode).json({message:message})
 })
